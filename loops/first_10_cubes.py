1cad6ca09d5e0d2352b43bf67f75c42cb9f2fa4c
Moez <65367982+moezmustafa@users.noreply.github.com>
2022-02-21 16:15:03 +0500

    Loops and recusrion

---
+++ loops/first_10_cubes.py
@@ -0,0 +1,6 @@
+
+for i in range(1,11):
+  cube = i**3
+
+  
+  print(cube)
---
+++ loops/sum.py
@@ -0,0 +1,9 @@
+def sum_return(n) :
+    sum =  0
+    for i in range(1,n):
+        
+        sum = sum + i
+
+    return sum
+
+print(sum_return(10))
